905cdf62ee16f240b359599fb5636b4a399c30a6
RichardRodri26 <richardmanuel811@gmail.com>
2024-11-04 12:17:22 -0500

    finish section 6

--- src/reports/index.ts
+++ src/reports/index.ts
@@ -1,4 +1,7 @@
+export * from './basic-chart-svg.report';
 export * from './countries.report';
 export * from './employment-letter-by-id.report';
 export * from './employment-letter.report';
 export * from './hello-world.report';
+export * from './order-by-id.report';
+export * from './statistics.report';
